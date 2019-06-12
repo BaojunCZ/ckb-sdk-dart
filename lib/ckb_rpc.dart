f47dacf9e08ca9192a71238f7990439f1c00633c
Bao Jun <baojun.cz@gmail.com>
2019-06-12 17:09:31 +0800

    chore: rename ckb_rpc ckb_types

--- lib/ckb_rpc.dart
+++ lib/ckb_rpc.dart
@@ -5,6 +5,6 @@ import 'package:http/http.dart' as http;
 import 'ckb_error.dart';
 import 'ckb_types.dart';
 
-part 'src/ckb-rpc/api_request.dart';
-part 'src/ckb-rpc/ckb_api_client.dart';
-part 'src/ckb-rpc/service_url.dart';
+part 'src/ckb_rpc/api_request.dart';
+part 'src/ckb_rpc/ckb_api_client.dart';
+part 'src/ckb_rpc/service_url.dart';
