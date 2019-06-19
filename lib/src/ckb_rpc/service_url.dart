1a47b0a52292dbe990d1a3a6316f05d3e10284fe
Bao Jun <baojun.cz@gmail.com>
2019-06-19 11:59:57 +0800

    chore: classify rpc url

--- lib/src/ckb_rpc/service_url.dart
+++ lib/src/ckb_rpc/service_url.dart
@@ -1,24 +1,40 @@
 part of 'package:ckb_sdk/ckb_rpc.dart';
 
 class ServiceUrl {
+  //Chain
   static final block = "get_block";
-  static final transaction = "get_transaction";
-  static final blockHash = "get_block_hash";
-  static final tipHeader = "get_tip_header";
-  static final cellsByLockHash = "get_cells_by_lock_hash";
-  static final liveCell = "get_live_cell";
-  static final tipBlockNumber = "get_tip_block_number";
-  static final localNodeInfo = "local_node_info";
-  static final sendTransaction = "send_transaction";
-  static final traceTransaction = "trace_transaction";
-  static final getTransactionTrace = "get_transaction_trace";
   static final getBlockByBlockNumber = "get_block_by_number";
+  static final blockHash = "get_block_hash";
+  static final cellsByLockHash = "get_cells_by_lock_hash";
   static final getCurrentEpoch = 'get_current_epoch';
   static final getEpochByNumber = 'get_epoch_by_number';
+  static final liveCell = "get_live_cell";
+  static final tipBlockNumber = "get_tip_block_number";
+  static final tipHeader = "get_tip_header";
+  static final transaction = "get_transaction";
+
+  //Experiment
+  static final computeTransactionHash = '_compute_transaction_hash';
+  static final computeCodeHash = '_compute_code_hash';
+  static final computeScriptHash = '_compute_script_hash';
+  static final dryRunTransaction = 'dry_run_transaction';
+
+  //Indexer
+  static final deindexLockHash = 'deindex_lock_hash';
+  static final getLiveCellsByLockHash = 'get_live_cells_by_lock_hash';
+  static final getLockHashIndexStates = 'get_lock_hash_index_states';
+  static final getTransactionsByLockHash = 'get_transactions_by_lock_hash';
+  static final indexLockHash = 'index_lock_hash';
+
+  //Net
   static final getPeers = 'get_peers';
+  static final localNodeInfo = "local_node_info";
+
+  //Pool
+  static final sendTransaction = "send_transaction";
   static final txPoolInfo = 'tx_pool_info';
+
+  //States
   static final getBlockchainInfo = 'get_blockchain_info';
   static final getPeersState = 'get_peers_state';
-  static final dryRunTransaction = 'dry_run_transaction';
-  static final computeTransactionHash = '_compute_transaction_hash';
 }
