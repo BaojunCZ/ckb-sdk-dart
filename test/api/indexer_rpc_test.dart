af3434dc796510d4f254fad033c82ca3920dc7de
Bao Jun <baojun.cz@gmail.com>
2019-07-01 10:42:20 +0800

    fix: indexer rpc error

--- test/api/indexer_rpc_test.dart
+++ test/api/indexer_rpc_test.dart
@@ -7,7 +7,7 @@ import 'package:ckb_sdk/ckb_types.dart';
 import 'package:test/test.dart';
 
 main() {
-  final apiClient = new CKBApiClient("http://127.0.0.1:8114");
+  final apiClient = new CKBApiClient("http://localhost:8114");
 
   test('deindex lock hash', () async {
     try {
@@ -23,7 +23,7 @@ main() {
   test('get lives cells by lock hash', () async {
     try {
       List<LiveCell> cells = await apiClient.getLiveCellsByLockHash(
-          "0xe94e4b509d5946c54ea9bc7500af12fd35eebe0d47a6b3e502127f94d34997ac", "0", "3");
+          "0xe94e4b509d5946c54ea9bc7500af12fd35eebe0d47a6b3e502127f94d34997ac", "1", "30");
       print(jsonEncode(cells));
     } catch (e) {
       print(e.toString());
@@ -55,7 +55,7 @@ main() {
   test('get index lock hash', () async {
     try {
       LockHashIndexState lockHashIndexStates = await apiClient.getIndexLockHash(
-          "0xe94e4b509d5946c54ea9bc7500af12fd35eebe0d47a6b3e502127f94d34997ac", "1024");
+          "0xe94e4b509d5946c54ea9bc7500af12fd35eebe0d47a6b3e502127f94d34997ac", "0");
       print(jsonEncode(lockHashIndexStates));
     } catch (e) {
       print(e.toString());
