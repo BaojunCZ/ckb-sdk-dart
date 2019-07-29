83ec45168451f509942984b719eee6818fad32c6
Bao Jun <baojun.cz@gmail.com>
2019-07-29 19:12:16 +0800

    feat: update address format generator

--- test/address/address_test.dart
+++ test/address/address_test.dart
@@ -20,23 +20,23 @@ main() {
 
   test('publicKey hash to address TestNet', () {
     var ckbAddress = CKBAddress(CKBNetwork.Testnet);
-    var address = ckbAddress
-        .generateFromPublicKey("390e45945092cbd81192cadd8ef268a398b49f521173d4c135f88893232b5c1af");
-    expect("ckt1q9gry5zgqnlmxcfvrjrm6m2ck2cmg9zev2rrpnnc3d940f", address);
+    var address = ckbAddress.generateFromPublicKey(
+        "0x024a501efd328e062c8675f2365970728c859c592beeefd6be8ead3d901330bc01");
+    expect("ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83", address);
   });
 
   test('publicKey hash to address MainNet', () {
     var ckbAddress = CKBAddress(CKBNetwork.Mainnet);
     var address = ckbAddress.generateFromPublicKey(
         "0x024a501efd328e062c8675f2365970728c859c592beeefd6be8ead3d901330bc01");
-    expect("ckb1q9gry5zgxmpjnmtrp4kww5r39frh2sm89tdt2l6vqdd7em", address);
+    expect("ckb1qyqrdsefa43s6m882pcj53m4gdnj4k440axqdt9rtd", address);
   });
 
   test('privateKey to address TestNet', () {
     var ckbAddress = CKBAddress(CKBNetwork.Testnet);
     var address = ckbAddress.generateFromPublicKey(bytesToHex(publicKeyFromPrivate(
         hex.decode("e79f3207ea4980b7fed79956d5934249ceac4751a4fae01a0f7c4a96884bc4e3"))));
-    expect(address, "ckt1q9gry5zgxmpjnmtrp4kww5r39frh2sm89tdt2l6v234ygf");
+    expect(address, "ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83");
   });
 
   test('address parse', () {
@@ -48,9 +48,9 @@ main() {
     expect(hash, payload);
   });
 
-  test('public key from address', () {
+  test('blake160 from address', () {
     var ckbAddress = CKBAddress(CKBNetwork.Testnet);
-    String address = "ckt1q9gry5zgxmpjnmtrp4kww5r39frh2sm89tdt2l6v234ygf";
+    String address = "ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83";
     String publicKey = ckbAddress.blake160FromAddress(address);
     expect(publicKey, "36c329ed630d6ce750712a477543672adab57f4c");
   });
